1c041f8c72e9594373fe18a87524c3d558ffac48
Makarand1999 <kshirsagarmakarand1999@gmail.com>
2024-07-29 01:30:55 +0530

    Added Backend Code

---
+++ lawyer-website-backend/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { registerUser, loginUser, getUser } = require('../controllers/userController');
+const auth = require('../middleware/authMiddleware');
+const router = express.Router();
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/me', auth, getUser);
+
+module.exports = router;
